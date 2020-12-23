82d5b1a1fae19c8b47b6ea0a9d749b3b3f5c73af
aarondegroote <aarondegroote7@gmail.com>
2020-12-23 14:41:45 +0100

    moving the paddle

--- src/main.py
+++ src/main.py
@@ -24,6 +24,9 @@ paddleA.rect.y = 200
 #This will be a list that will contain all the sprites we intend to use in our game.
 all_sprites_list = pygame.sprite.Group()
 
+# Add thepaddles to the list of sprites
+all_sprites_list.add(paddleA)
+
 # The loop will carry on until the user exit the game (e.g. clicks the close button).
 carryOn = True
  
@@ -36,6 +39,24 @@ while carryOn:
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
               carryOn = False # Flag that we are done so we exit this loop
+        elif event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_x: #pressing x to quit game
+                    carryOn=False
+
+    #Moving the paddles when the user uses the arrow keys (player A) or "W/S" keys (player B) 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_z]:
+        paddleA.moveUp(5)
+    if keys[pygame.K_s]:
+        paddleA.moveDown(5)
+    # if keys[pygame.K_UP]:
+    #     snake.moveUp(5)
+    # if keys[pygame.K_DOWN]:
+    #     snake.moveDown(5)    
+    # if keys[pygame.K_UP]:
+    #     snake.moveLeft(5)
+    # if keys[pygame.K_DOWN]:
+    #     snake.moveRight(5) 
 
     # --- Game logic should go here
     all_sprites_list.update()
@@ -52,6 +73,10 @@ while carryOn:
     pygame.draw.line(screen, BLUE, [0, 0], [0,620], 10)
     pygame.draw.line(screen, BLUE, [0, 617], [820,617], 10)
     pygame.draw.line(screen, BLUE, [817, 0], [817,620], 10)
+
+    #drawing al the sprites in one go on the screen
+    all_sprites_list.draw(screen) 
+
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
      
