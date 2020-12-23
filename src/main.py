da92b69790522d137acbabe4b2c18fa2a1fffc10
Simon Mestdagh <simonmestdagh2001@gmail.com>
2020-12-23 14:45:49 +0100

    snake

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 # Import the pygame library and initialise the game engine
 import pygame
+from snake import Snake
+
 pygame.init()
  
 # Define some colors
@@ -14,7 +16,17 @@ BLUE = (50, 153, 213)
 size = (800+20, 600+20)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong")
- 
+
+paddleA = Paddle(WHITE, 10, 100)
+paddleA.rect.x = 20
+paddleA.rect.y = 200
+
+#This will be a list that will contain all the sprites we intend to use in our game.
+all_sprites_list = pygame.sprite.Group()
+
+# Add thepaddles to the list of sprites
+all_sprites_list.add(paddleA)
+
 # The loop will carry on until the user exit the game (e.g. clicks the close button).
 carryOn = True
  
@@ -27,21 +39,44 @@ while carryOn:
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
               carryOn = False # Flag that we are done so we exit this loop
- 
+        elif event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_x: #pressing x to quit game
+                    carryOn=False
+
+    #Moving the paddles when the user uses the arrow keys (player A) or "W/S" keys (player B) 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_z]:
+        paddleA.moveUp(5)
+    if keys[pygame.K_s]:
+        paddleA.moveDown(5)
+    # if keys[pygame.K_UP]:
+    #     snake.moveUp(5)
+    # if keys[pygame.K_DOWN]:
+    #     snake.moveDown(5)    
+    # if keys[pygame.K_UP]:
+    #     snake.moveLeft(5)
+    # if keys[pygame.K_DOWN]:
+    #     snake.moveRight(5) 
+
     # --- Game logic should go here
- 
+    all_sprites_list.update()
  
  
     # --- Drawing code should go here
     # First, clear the screen to black. 
-    screen.fill(BLACK)
+    
     #Draw the net
+    screen.fill(BLACK)
 
 
     pygame.draw.line(screen, BLUE, [0, 0], [820,0], 10)
     pygame.draw.line(screen, BLUE, [0, 0], [0,620], 10)
-    pygame.draw.line(screen, BLUE, [820, 620], [820,0], 10)
-    pygame.draw.line(screen, BLUE, [820, 620], [0,620], 10)
+    pygame.draw.line(screen, BLUE, [0, 617], [820,617], 10)
+    pygame.draw.line(screen, BLUE, [817, 0], [817,620], 10)
+
+    #drawing al the sprites in one go on the screen
+    all_sprites_list.draw(screen) 
+
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
      
