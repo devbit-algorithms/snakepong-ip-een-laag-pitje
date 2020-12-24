1f07ccf03ce6b77a2091652840b1043d993cad6c
Simon Mestdagh <simonmestdagh2001@gmail.com>
2020-12-24 11:42:17 +0100

    added snake but nog object oriented

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 # Import the pygame library and initialise the game engine
 import pygame
+import random
+import time
 
 from paddle import Paddle
+from ball import Ball
 
-from snake import Snake
+#from snake import Snake
 
 
 pygame.init()
@@ -15,9 +18,11 @@ BLACK = (0, 0, 0)
 RED = (213, 50, 80)
 GREEN = (0, 255, 0)
 BLUE = (50, 153, 213)
- 
+
+width = 700
+height = 500
 # Open a new window
-size = (800+20, 600+20)
+size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong")
 
@@ -25,18 +30,40 @@ paddleA = Paddle(WHITE, 10, 100)
 paddleA.rect.x = 20
 paddleA.rect.y = 200
 
+ball = Ball(WHITE, 10, 10)
+ball.rect.x = 345
+ball.rect.y = 195
+
 #This will be a list that will contain all the sprites we intend to use in our game.
 all_sprites_list = pygame.sprite.Group()
 
 # Add thepaddles to the list of sprites
 all_sprites_list.add(paddleA)
+all_sprites_list.add(ball)
 
 # The loop will carry on until the user exit the game (e.g. clicks the close button).
 carryOn = True
  
 # The clock will be used to control how fast the screen updates
 clock = pygame.time.Clock()
+
+#snake
+snake_block = 10
+snake_speed = 15
+x1 = width / 2
+y1 = height / 2
+x1_change = 0
+y1_change = 0
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(screen, WHITE, [x[0], x[1], snake_block, snake_block])
+
+foodx = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
+foody = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
  
+snake_List = []
+Length_of_snake = 1
 # -------- Main Program Loop -----------
 while carryOn:
     # --- Main event loop
@@ -48,44 +75,94 @@ while carryOn:
                     carryOn=False
 
     #Moving the paddles when the user uses the arrow keys (player A) or "W/S" keys (player B) 
+    
+    
+    screen.fill(BLACK)
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_z]:
         paddleA.moveUp(5)
-    if keys[pygame.K_s]:
+    elif keys[pygame.K_s]:
         paddleA.moveDown(5)
-    # if keys[pygame.K_UP]:
-    #     snake.moveUp(5)
-    # if keys[pygame.K_DOWN]:
-    #     snake.moveDown(5)    
-    # if keys[pygame.K_UP]:
-    #     snake.moveLeft(5)
-    # if keys[pygame.K_DOWN]:
-    #     snake.moveRight(5) 
+    elif keys[pygame.K_UP]:
+        y1_change = -snake_block
+        x1_change = 0
+    elif keys[pygame.K_DOWN]:
+        y1_change = snake_block
+        x1_change = 0
+    elif keys[pygame.K_LEFT]:
+        x1_change = -snake_block
+        y1_change = 0
+    elif keys[pygame.K_RIGHT]:
+        x1_change = snake_block
+        y1_change = 0
+
+    if x1 >= width or x1 < 0 or y1 >= height or y1 < 0:
+        game_close = True
+    
+    x1 += x1_change
+    y1 += y1_change
+
+    pygame.draw.rect(screen, GREEN, [foodx, foody, snake_block, snake_block])
+
+    snake_Head = []
+    snake_Head.append(x1)
+    snake_Head.append(y1)
+    snake_List.append(snake_Head)
+
+    if len(snake_List) > Length_of_snake:
+        del snake_List[0]
+ 
+    for x in snake_List[:-1]:
+        if x == snake_Head:
+            game_close = True
+
+    
 
     # --- Game logic should go here
     all_sprites_list.update()
  
+
+    if ball.rect.x>=690:
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.x<=0:
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.y>490:
+        ball.velocity[1] = -ball.velocity[1]
+    if ball.rect.y<0:
+        ball.velocity[1] = -ball.velocity[1] 
  
+
+    #Detect collisions between the ball and the paddles
+    if pygame.sprite.collide_mask(ball, paddleA):
+      ball.bounce()
+    
     # --- Drawing code should go here
     # First, clear the screen to black. 
-    
+ 
     #Draw the net
-    screen.fill(BLACK)
+    
+    our_snake(snake_block, snake_List)
 
-
-    pygame.draw.line(screen, BLUE, [0, 0], [820,0], 10)
-    pygame.draw.line(screen, BLUE, [0, 0], [0,620], 10)
-    pygame.draw.line(screen, BLUE, [0, 617], [820,617], 10)
-    pygame.draw.line(screen, BLUE, [817, 0], [817,620], 10)
+    # pygame.draw.line(screen, BLUE, [0, 0], [700,0], 10)
+    # pygame.draw.line(screen, BLUE, [0, 0], [0,500], 10)
+    # pygame.draw.line(screen, BLUE, [0, 617], [820,617], 10)
+    # pygame.draw.line(screen, BLUE, [817, 0], [817,620], 10)
 
     #drawing al the sprites in one go on the screen
     all_sprites_list.draw(screen) 
 
+    if x1 == foodx and y1 == foody:
+        foodx = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
+        foody = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
+        Length_of_snake += 1
+
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
+
      
     # --- Limit to 60 frames per second
-    clock.tick(60)
+    clock.tick(30)
  
 #Once we have exited the main program loop we can stop the game engine:
 pygame.quit()
--- src/snake.py
+++ src/snake.py
@@ -1,33 +1,178 @@
 import pygame
-BLACK = (0,0,0)
- 
-class Paddle(pygame.sprite.Sprite):
-    #This class represents a paddle. It derives from the "Sprite" class in Pygame.
-    
-    def __init__(self, color, width, height):
-        # Call the parent class (Sprite) constructor
+import random
+
+# --- Globals ---
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+
+# Screen size
+height = 600
+width = 600
+
+# Margin between each segment
+segment_margin = 3
+
+# Set the width and height of each snake segment
+segment_width = min(height, width) / 40 - segment_margin
+segment_height = min(height, width) / 40 - segment_margin
+
+# Set initial speed
+x_change = segment_width + segment_margin
+y_change = 0
+
+
+class Snake():
+    """ Class to represent one snake. """
+
+    # Constructor
+    def __init__(self):
+        self.segments = []
+        self.spriteslist = pygame.sprite.Group()
+        for i in range(15):
+            x = (segment_width + segment_margin) * 30 - (segment_width + segment_margin) * i
+            y = (segment_height + segment_margin) * 2
+            segment = Segment(x, y)
+            self.segments.append(segment)
+            self.spriteslist.add(segment)
+
+    def move(self):
+        # Figure out where new segment will be
+        x = self.segments[0].rect.x + x_change
+        y = self.segments[0].rect.y + y_change
+
+        # Don't move off the screen
+        # At the moment a potential move off the screen means nothing happens, but it should end the game
+        if 0 <= x <= width - segment_width and 0 <= y <= height - segment_height:
+
+        # Insert new segment into the list
+            segment = Segment(x, y)
+            self.segments.insert(0, segment)
+            self.spriteslist.add(segment)
+        # Get rid of last segment of the snake
+        # .pop() command removes last item in list
+            old_segment = self.segments.pop()
+            self.spriteslist.remove(old_segment)
+        else:
+            return
+        self.eat()
+
+
+
+
+    def grow(self):
+        x = self.segments[0].rect.x + x_change
+        y = self.segments[0].rect.y + y_change
+        segment = Segment(x, y)
+        self.segments.insert(len(self.segments), segment)
+        self.spriteslist.add(segment)
+
+
+class Segment(pygame.sprite.Sprite):
+    """ Class to represent one segment of a snake. """
+
+    # Constructor
+    def __init__(self, x, y):
+        # Call the parent's constructor
         super().__init__()
-        
-        # Pass in the color of the paddle, and its x and y position, width and height.
-        # Set the background color and set it to be transparent
-        self.image = pygame.Surface([width, height])
-        self.image.fill(BLACK)
-        self.image.set_colorkey(BLACK)
- 
-        # Draw the paddle (a rectangle!)
-        pygame.draw.rect(self.image, color, [0, 0, width, height])
-        
-        # Fetch the rectangle object that has the dimensions of the image.
+
+        # Set height, width
+        self.image = pygame.Surface([segment_width, segment_height])
+        self.image.fill(WHITE)
+
+        # Set top-left corner of the bounding rectangle to be the passed-in location.
         self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
-    def moveUp(self, pixels):
-        self.rect.y -= pixels
-	    #Check that you are not going too far (off the screen)
-        if self.rect.y < 0:
-            self.rect.y = 0
+foodx = random.choice([i for i in range(0, 600, 15)])
+foody = random.choice([i for i in range(0, 600, 15)])
 
-    def moveDown(self, pixels):
-        self.rect.y += pixels
-        #Check that you are not going too far (off the screen)
-        if self.rect.y > 550:
-            self.rect.y = 550
+class Food():
+    def __init__(self):
+        self.foodlists = []
+        self.foodlist = pygame.sprite.Group()
+
+        fooditem = Food_item(foodx, foody)
+        self.foodlists.append(fooditem)
+        self.foodlist.add(fooditem)
+
+    def replenish(self):
+
+        newfoodx = random.choice([i for i in range(0, 600, 15)])
+        newfoody = random.choice([i for i in range(0, 600, 15)])
+        fooditem = Food_item(newfoodx, newfoody)
+        self.foodlists.append(fooditem)
+        self.foodlist.add(fooditem)
+
+
+
+class Food_item(pygame.sprite.Sprite):
+    def __init__(self, fx, fy):
+        # Call the parent's constructor
+        super().__init__()
+        # Set height, width
+        self.image = pygame.Surface([segment_width, segment_height])
+        self.image.fill(RED)
+
+        # Set top-left corner of the bounding rectangle to be the passed-in location.
+        self.rect = self.image.get_rect()
+        self.rect.x = fx
+        self.rect.y = fy
+
+
+""" # Call this function so the Pygame library can initialize itself
+pygame.init()
+
+# Create a 600x600 sized screen
+screen = pygame.display.set_mode([width, height])
+
+# Set the title of the window
+pygame.display.set_caption('Snake Game')
+
+# Create an initial snake
+my_snake = Snake()
+food = Food()
+
+clock = pygame.time.Clock()
+done = False """
+
+""" while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+        # Set the direction based on the key pressed
+        # We want the speed to be enough that we move a full
+        # segment, plus the margin.
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = (segment_width + segment_margin) * -1
+                y_change = 0
+            if event.key == pygame.K_RIGHT:
+                x_change = (segment_width + segment_margin)
+                y_change = 0
+            if event.key == pygame.K_UP:
+                x_change = 0
+                y_change = (segment_height + segment_margin) * -1
+            if event.key == pygame.K_DOWN:
+                x_change = 0
+                y_change = (segment_height + segment_margin)
+
+    # move snake one step
+    my_snake.move()
+    # -- Draw everything
+    # Clear screen
+    screen.fill(BLACK)
+    my_snake.spriteslist.draw(screen)
+    food.foodlist.draw(screen)
+
+    # Flip screen
+    pygame.display.flip()
+
+    # Pause
+    clock.tick(5) """
+
+pygame.quit()
